f1a9df75791b15c82f3cd0b718366bd9ef9a8d3a
DRK-In <drk.mobile03@gmail.com>
2021-01-24 16:18:26 +0100

    .

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const close_btn = document.getElementById('toggle')
 const navbar = document.getElementById('navbar')
-const logo = document.getElementById('logo')
+
 
 
 close_btn.addEventListener('click', () => {
     navbar.classList.toggle('show')
-    position()
+
 
 
 })
